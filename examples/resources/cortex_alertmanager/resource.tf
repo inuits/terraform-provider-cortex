bfd466aa60da794ba2175e44a6038e6ec6255cf0
Julien Pivotto <roidelapluie@inuits.eu>
2021-02-03 14:37:13 +0100

    Remove mixed example
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- examples/resources/cortex_alertmanager/resource.tf
+++ examples/resources/cortex_alertmanager/resource.tf
@@ -24,14 +24,3 @@ inhibit_rules:
     equal: ['alertname', 'dev', 'instance']
 EOT
 }
-
-resource "cortex_rules" "watchdog" {
-  namespace = "default"
-  content   = <<EOT
-name: watchdog
-rules:
-- alert: Watchdog
-  expr: vector(1)
-  for: 20m
-EOT
-}
